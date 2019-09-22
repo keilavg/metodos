ca2ee8073fbbf640bad1805395ce51df7b0e5f36
Keila Viana <keilavg72@gmail.com>
2019-09-22 11:19:06 -0300

    Adicionando o arquivo pilhas.c que contem as funçoes, e realizando os testes para ver se o programa esta funcionando

---
+++ lista2/questao1/teste.c
@@ -0,0 +1,21 @@
+#include "pilha.c"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+
+    Pilha* p;
+    p = cria();
+    pilha_vazia(p);
+    push1(p, "keila\n");
+    
+    push1(p, "viana \n");
+    imprime(p);
+
+    pop(p);
+    imprime(p);
+    pop(p);
+    imprime(p);
+}
