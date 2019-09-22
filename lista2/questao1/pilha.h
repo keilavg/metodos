ff9d643970e7c3c2125bb2edb6a0b5ca6fd06520
Keila Viana <keilavg72@gmail.com>
2019-09-22 09:02:38 -0300

    Adicionando API da 1 questao

---
+++ lista2/questao1/pilha.h
@@ -0,0 +1,19 @@
+
+
+typedef struct lista
+{
+    char info[100];
+    struct lista* prox;
+    
+}Lista;
+
+typedef struct pilha {
+    Lista* topo;
+} Pilha;
+
+Pilha* cria();
+int pilha_vazia(Pilha* p);
+void push(Pilha * p);
+void pop(Pilha * p);
+void imprime(Pilha *p);
+void libera(Pilha ** p);
